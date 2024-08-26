b13c2a1a0b2ecd843afe7d7d03a8bcebf7e8888f
Anurag Chaturvedi <anuragcodejam@gmail.com>
2024-08-27 00:12:33 +0100

    6. Added command line argument for username

--- src/index.ts
+++ src/index.ts
@@ -6,19 +6,23 @@ import * as _ from 'lodash';
 console.log("hello");
 
 // const USERNAME = "annu4git";
-const USERNAME = "koushikkothagal";
+// const USERNAME = "koushikkothagal";
 
-var service = new GithubApiService();
-service.getUserInfo(USERNAME, (user : User) => {
-    service.getRepos(USERNAME, (repos : Repo[]) => {
-        console.log(" Total repos : " + repos.length);
-        let sortedRepos = _.sortBy(repos, [function(repo) { return repo.forkCount * -1; }]);
-        let filteredRepos = _.take(sortedRepos, 5);
-        user.repos = filteredRepos;
-        console.log(user);
+console.log(process.argv);
+
+if(process.argv.length < 3) {
+    console.log('Enter username in command line arguments');
+} else {
+    const USERNAME = process.argv[2];
+
+    var service = new GithubApiService();
+    service.getUserInfo(USERNAME, (user : User) => {
+        service.getRepos(USERNAME, (repos : Repo[]) => {
+            console.log(" Total repos : " + repos.length);
+            let sortedRepos = _.sortBy(repos, [function(repo) { return repo.forkCount * -1; }]);
+            let filteredRepos = _.take(sortedRepos, 5);
+            user.repos = filteredRepos;
+            console.log(user);
+        });
     });
-});
-
-
-
-
+}
