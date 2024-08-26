4387a9d5d6db6b3e7679b0d2c53ebf3b5dd97412
Anurag Chaturvedi <anuragcodejam@gmail.com>
2024-08-26 23:13:42 +0100

    2. Added a callback function
    Since HTTP call is async, added callback function, now the user details are getting printed at consumer side

--- src/GithubApiService.ts
+++ src/GithubApiService.ts
@@ -1,21 +1,20 @@
 import * as request from 'request';
 import { User } from './User';
 
+const OPTIONS : any = {
+    headers : {
+        'User-Agent' : 'request'
+    },
+    json : true
+}
+
 export class GithubApiService {
 
-    getUserInfo(userName : string) {
-        let options : any = {
-            headers : {
-                'User-Agent' : 'request'
-            },
-            json : true
-        }
-
-        request.get('https://api.github.com/users/' + userName, options, (error, response, body) => {
-            // var user = new User(JSON.parse(body));
-            var user = new User(body);
-            console.log(user);
-            return user;
+    getUserInfo(userName : string, callbackFunction : (user : User) => any) {
+        request.get('https://api.github.com/users/' + userName, OPTIONS, (error, response, body) => {
+            let user = new User(body);
+            // console.log(user);
+            callbackFunction(user);
         })
     }
 
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { GithubApiService } from "./GithubApiService";
+import { User } from "./User";
 
 console.log("hello");
 
 var service = new GithubApiService();
-var user = service.getUserInfo("annu4git");
+var user = service.getUserInfo("annu4git", (user : User) => {
+    console.log(user);
+});
